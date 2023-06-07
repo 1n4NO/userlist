94dd6424912a1462668bb9f5500a1150ebd5965c
P S <128357922+1n4NO@users.noreply.github.com>
2023-06-08 03:18:51 +0530

    added test to check fetch

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App'
 import store from './app/store'
 import { Provider } from 'react-redux'
+import 'isomorphic-fetch'
 
 test('shows loading', () => {
   render(<Provider store={store}>
@@ -9,4 +10,10 @@ test('shows loading', () => {
   </Provider>,)
   const linkElement = screen.getByText(/Loading!/);
   expect(linkElement).toBeInTheDocument()
+});
+
+test('test fetch call', async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/1');
+  const result = await res.json();
+  expect(result.name).toBe('Leanne Graham');  // Success!
 });
